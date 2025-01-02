1b24da7dd97938ea3fe15e2d3f6198297fa31791
mkurzych <marta.kurzych@wp.pl>
2025-01-02 20:39:37 +0100

    implemented in progress & done

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"github.com/charmbracelet/bubbles/list"
 	tea "github.com/charmbracelet/bubbletea"
+	"github.com/charmbracelet/lipgloss"
 	"os"
 )
 
@@ -13,6 +14,23 @@ const (
 	todo status = iota
 	inProgress
 	done
+)
+
+const divisor = 3
+
+// style
+
+var (
+	columnStyle = lipgloss.NewStyle().
+		Padding(1, 2).
+		Border(lipgloss.RoundedBorder()).
+		BorderForeground(lipgloss.Color("241"))
+	focusedStyle = lipgloss.NewStyle().
+		Padding(1, 2).
+		Border(lipgloss.RoundedBorder()).
+		BorderForeground(lipgloss.Color("62"))
+	helpStyle = lipgloss.NewStyle().
+		Foreground(lipgloss.Color("241"))
 )
 
 // custom item
@@ -40,21 +58,57 @@ func (t Task) Description() string {
 // main model
 
 type Model struct {
-	list list.Model
+	lists   []list.Model
+	focused status
+	err     error
+	loaded  bool
+	quit    bool
 }
 
 func New() *Model {
 	return &Model{}
 }
 
+// go to next list
+
+func (m *Model) Next() {
+	if m.focused == done {
+		m.focused = todo
+	} else {
+		m.focused++
+	}
+}
+
+// go to previous list
+
+func (m *Model) Prev() {
+	if m.focused == todo {
+		m.focused = done
+	} else {
+		m.focused--
+	}
+}
+
 // TODO: call this on tea.WindowSizeMsg
-func (m *Model) initList(width, height int) {
-	m.list = list.New([]list.Item{}, list.NewDefaultDelegate(), width, height)
-	m.list.Title = "To Do"
-	m.list.SetItems([]list.Item{
-		Task{status: todo, title: "Write a blog post", description: "Write a blog post about Bubble Tea"},
+func (m *Model) initLists(width, height int) {
+	defaultList := list.New([]list.Item{}, list.NewDefaultDelegate(), width/divisor-5, height-5)
+	defaultList.SetShowHelp(false)
+	m.lists = []list.Model{defaultList, defaultList, defaultList}
+	// init todos list
+	m.lists[todo].Title = "To Do"
+	m.lists[todo].SetItems([]list.Item{
 		Task{status: todo, title: "Walk the dog", description: "Take the dog for a walk around the block"},
 		Task{status: todo, title: "Buy groceries", description: "Buy milk, eggs, and bread"},
+	})
+	// init in progress list
+	m.lists[inProgress].Title = "In Progress"
+	m.lists[inProgress].SetItems([]list.Item{
+		Task{status: inProgress, title: "Write a blog post", description: "Write a blog post about Bubble Tea"},
+	})
+	// init done list
+	m.lists[done].Title = "Done"
+	m.lists[done].SetItems([]list.Item{
+		Task{status: done, title: "Try out Bubble tea", description: "Follow a tutorial and try out Bubble Tea"},
 	})
 }
 
@@ -65,15 +119,64 @@ func (m *Model) Init() tea.Cmd {
 func (m *Model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 	switch msg := msg.(type) {
 	case tea.WindowSizeMsg:
-		m.initList(msg.Width, msg.Height)
+		if !m.loaded {
+			columnStyle.Width(msg.Width / divisor)
+			focusedStyle.Width(msg.Width / divisor)
+			columnStyle.Height(msg.Height - 5)
+			focusedStyle.Height(msg.Height - 5)
+			m.initLists(msg.Width, msg.Height)
+			m.loaded = true
+		}
+	case tea.KeyMsg:
+		switch msg.String() {
+		case "q", "ctrl+c":
+			m.quit = true
+			return m, tea.Quit
+		case "left", "h":
+			m.Prev()
+		case "right", "l":
+			m.Next()
+		}
 	}
 	var cmd tea.Cmd
-	m.list, cmd = m.list.Update(msg)
+	m.lists[m.focused], cmd = m.lists[m.focused].Update(msg)
 	return m, cmd
 }
 
 func (m *Model) View() string {
-	return m.list.View()
+	if m.quit {
+		return ""
+	}
+	if m.loaded {
+		todoView := m.lists[todo].View()
+		inProgressView := m.lists[inProgress].View()
+		doneView := m.lists[done].View()
+		switch m.focused {
+		case inProgress:
+			return lipgloss.JoinHorizontal(
+				lipgloss.Left,
+				columnStyle.Render(todoView),
+				focusedStyle.Render(inProgressView),
+				columnStyle.Render(doneView),
+			)
+		case done:
+			return lipgloss.JoinHorizontal(
+				lipgloss.Left,
+				columnStyle.Render(todoView),
+				columnStyle.Render(inProgressView),
+				focusedStyle.Render(doneView),
+			)
+		default:
+			return lipgloss.JoinHorizontal(
+				lipgloss.Left,
+				focusedStyle.Render(todoView),
+				columnStyle.Render(inProgressView),
+				columnStyle.Render(doneView),
+			)
+		}
+	} else {
+		return "Loading..."
+	}
 }
 
 func main() {
